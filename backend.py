aa130fd5bac3141be77b3744dfd5c4de47963ffb
Divyasoni2906 <divyasoni2906@gmail.com>
2026-06-06 20:00:39 +0530

    added cors

--- backend.py
+++ backend.py
@@ -22,7 +22,9 @@ app = FastAPI(title="Security RAG API")
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],  # React dev servers
+    allow_origins=[
+        "*"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
